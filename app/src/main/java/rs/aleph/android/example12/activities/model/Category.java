bdfd9093a5f8d2b3383c80659d41902b23f84067
Tijana <tijanalaz@gmail.com>
2018-01-28 16:59:26 +0100

    Upload new project version Termin14 fix #3

---
+++ app/src/main/java/rs/aleph/android/example12/activities/model/Category.java
@@ -0,0 +1,9 @@
+package rs.aleph.android.example12.activities.model;
+
+/**
+ * Created by tijana on 28.1.18..
+ */
+
+public enum Category {
+     SIDE_DISH, MAIN_DISH, DESERT;
+}
---
+++ app/src/main/java/rs/aleph/android/example12/activities/model/Meal.java
@@ -0,0 +1,110 @@
+package rs.aleph.android.example12.activities.model;
+
+import java.util.Locale;
+
+/**
+ * Created by androiddevelopment on 27.1.18..
+ */
+
+public class Meal {
+    private int id;
+    private String image;
+    private String name;
+    private String description;
+    private Category category;
+    private String ingredients;
+    private int calories;
+    private double price;
+
+    public Meal() {
+
+    }
+
+    public Meal(String image, String name, String description, Category category, String ingredients, int calories, double price) {
+        this.image = image;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.ingredients = ingredients;
+        this.calories = calories;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", image='" + image + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", ingredients=" + ingredients + '\'' +
+                ", calories=" + calories + '\'' +
+                ", price=" + price + '\'' +
+                '}';
+    }
+}
