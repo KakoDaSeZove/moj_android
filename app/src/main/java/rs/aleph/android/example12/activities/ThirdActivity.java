1c317e836c50b52febdcb39cdac4d7d756010711
Tijana <tijanalaz@gmail.com>
2018-01-28 11:58:49 +0100

    Initial code for Andorid exercises
    Base exercise is from class 13.

---
+++ app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
@@ -0,0 +1,47 @@
+package rs.aleph.android.example12.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by androiddevelopment on 23.1.18..
+ */
+
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
